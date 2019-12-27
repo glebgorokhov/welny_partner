812bd7764952e178f3f8dfb63ffef1c14534142d
Gleb Gorokhov <gg@verstaburo.com>
2019-12-27 14:01:01 +0300

    Выравнивание заголовков

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,4 +9,33 @@
  * Так же доступа к блокам, которые собираются с помощью вебпака не будет.
  */
 
+function labelsHeights () {
+  $(document).find('.inputbox__label').height('auto');
+
+  $(document).find('.inputbox__label').each(function () {
+    const
+      el = $(this),
+      height = el.height(),
+      pos = el.offset().top;
+
+    let maxHeight = height;
+
+    $(document).find('.inputbox__label').each(function () {
+      const
+        thisPos = $(this).offset().top,
+        thisHeight = $(this).height();
+
+      if (thisPos !== pos) return;
+      if (thisHeight > maxHeight) maxHeight = thisHeight;
+
+      $(this).height(maxHeight);
+    });
+
+    el.height(maxHeight);
+  });
+}
+
+labelsHeights();
+
+$(window).resize(labelsHeights);
 /* eslint-enable */
